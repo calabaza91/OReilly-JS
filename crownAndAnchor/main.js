37b333714867872082b54dc0b72313da482f8e15
Caleb Cox <calebcox2010@gmail.com>
2019-03-28 15:45:31 -0500

    Add functions and better explanation of game

--- crownAndAnchor/main.js
+++ crownAndAnchor/main.js
@@ -1,5 +1,11 @@
 let funds = 50; //Starting collections
 
+//Functions
+function rand(m,n){
+  return m + Math.floor((n - m + 1) * Math.random());
+}
+
+
 while(funds > 0 || funds < 100){
   //place bets
 
