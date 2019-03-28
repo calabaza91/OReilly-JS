2a48a91952b61199eb42c235c3170177ec5e427c
Caleb Cox <calebcox2010@gmail.com>
2019-03-27 21:43:35 -0500

    Install underscore module for demo1

--- demos/demo1.js
+++ demos/demo1.js
@@ -1,18 +1,27 @@
 'use strict';
+//use npm underscore
+const _ = require('underscore');
+
 const sentences = [
   {subject:'JavaScript', verb:'is', object:'great'},
   {subject:'Elephants', verb:'are', object:'large'}
 ];
 
-//es6 feature: object destructuring
+//Removed for the use of underscore
+/*es6 feature: object destructuring
 function say({subject, verb, object}){
 
-//es6 feature: template strings
-//note that quotes below are backticks (`), not single quotes (')
-  console.log(`${subject} ${verb} ${object}`);
-}
 
+es6 feature: template strings
+note that quotes below are backticks (`), not single quotes (')
+  console.log(`${subject} ${verb} ${object}`);*/
+
+
+//Pluck gets the verb out of the sentences
+const verbs = _.pluck(sentences, 'verb');
 //es6 feature: for..of
-for (let s of sentences) {
-  say(s);
+for (let verb of verbs) {
+  console.log(`found verb: ${verb}`)
+
+  //say(s);
 }
